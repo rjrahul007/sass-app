b6f57a0932978e7e61e410d9dd2bca7b3dd40bbc
rjrahul007 <rjrahool007@gmail.com>
2025-05-29 16:24:04 +0530

    Implement companion table & CTA

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
-import { Button } from '@/components/ui/button'
-import Navbar from '@/components/Navbar'
+import CompanionCard from '@/components/CompanionCard'
+import CTA from '@/components/CTA'
+import CompanionsList from '@/components/CompanionsList'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS</h1>
-      <Button>Let's get started</Button>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard 
+        id='123' 
+        name="Neura the Brainy Explorer" 
+        topic="Neural Network of the Brain" subject="science" 
+        duration={30} 
+        color="#ffda6e"/>
+        <CompanionCard id='456' 
+        name="Countsy the Number Wizard" 
+        topic="Derivatives & Integrals" subject="Maths" 
+        duration={30} 
+        color="#efd0ff"/>
+        <CompanionCard id='789' 
+        name="Verba the Vocabulary Virtuoso"
+        topic="English Vocabulary" subject="English"
+        duration={30}
+        color="#bde7ff"/> 
+  
+      </section>
+      <section className="home-section">
+        <CompanionsList
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full "
+        
+        />
+        <CTA/>
+      </section>
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import Navitems from './Navitems'
