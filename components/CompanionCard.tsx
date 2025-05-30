eaaaa265b409bafef96119074275b1633afb56e3
rjrahul007 <rjrahool007@gmail.com>
2025-05-30 15:06:31 +0530

    Update minutes in companionCard

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -26,7 +26,7 @@ const CompanionCard = ({id, name, topic, subject, duration, color}: CompanionCar
         <p className='text-sm '>{topic}</p>
         <div className="flex itmes-center gap-2">
             <Image src='/icons/clock.svg' alt="duration" width={13.5} height={13.5}/>
-            <p className="text-sm">{duration} mins duration</p>
+            <p className="text-sm">{duration} minutes</p>
         </div>
        <Link
         href={`/companions/${id}`}
@@ -34,7 +34,6 @@ const CompanionCard = ({id, name, topic, subject, duration, color}: CompanionCar
         >
           <button className='btn-primary w-full justify-center'>Launch Lesson</button>
         </Link>
-    
     </article>
   )
 }
