e44e22b05b975cee4a0beff391cb7a5d7292b8f7
rjrahul007 <rjrahool007@gmail.com>
2025-06-11 14:55:05 +0530

    Install jsmastery/utils

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+'use client';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
+// import { useRouter } from 'next/router';
+import React, { useState } from 'react'
+import Image from 'next/image';
 
 const SearchInput = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const query = searchParams.get('topic') || '';
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    
+
   return (
-    <div>
-      
-    </div>
+  <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+    <Image src="/icons/search.svg" alt="Search Icon" width={15} height={15} />
+    <input
+      type="text"
+      placeholder="Search companions..."
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      className="w-full outline-none"
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          if (searchQuery.trim() !== '') {
+            router.push(`${pathname}?topic=${encodeURIComponent(searchQuery.trim())}`);
+          }
+        }
+      }}
+    />
+  </div>
   )
 }
 
